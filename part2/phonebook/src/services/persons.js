2e445a270b5884229a0076b987aa64aa56df0fa9
Saif Ibn Reza <139000073+saifxbot@users.noreply.github.com>
2026-01-06 14:23:34 +0200

    Update baseUrl to include /api path prefix

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,25 +1,21 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  return axios.get(baseUrl)
 }
 
-const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
+const create = newObject => {
+  return axios.post(baseUrl, newObject)
 }
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
+  return axios.put(`${baseUrl}/${id}`, newObject)
 }
 
-const remove = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`)
-  return request.then(response => response.data)
+const remove = id => {
+  return axios.delete(`${baseUrl}/${id}`)
 }
 
 export default { getAll, create, update, remove }
